8547096938c1960eb80190f7498c25c43e2fdf63
Arnur <arnurarykbayev@mail.ru>
2022-10-15 22:12:08 +0600

    fix users modal info

--- js/app.js
+++ js/app.js
@@ -9,11 +9,30 @@ document.addEventListener("DOMContentLoaded", async function () {
   const exampleModal = document.getElementById("exampleModal");
   exampleModal.addEventListener("show.bs.modal", (event) => {
     const card = event.relatedTarget;
-    const recipient = card.getAttribute("data-bs-whatever");
-    const modalTitle = exampleModal.querySelector(".modal-title");
-    const modalInfoPhone = exampleModal.querySelector(".modal-info-phone");
 
-    modalTitle.textContent = `${recipient}`;
+    const userFullName = card.getAttribute("data-bs-fullname");
+    const userPhone = card.getAttribute("data-bs-phone");
+    const userEmail = card.getAttribute("data-bs-email");
+    const userCompName = card.getAttribute("data-bs-company-name");
+    const userCompanyTitle = card.getAttribute("data-bs-company-title");
+    const userCompanyDepartment = card.getAttribute("data-bs-company-department");
+    const userAddress = card.getAttribute("data-bs-address");
+
+    const modalTitle = exampleModal.querySelector(".modal-title");
+    const modalPhone = exampleModal.querySelector(".modal-info-phone");
+    const modalEmail = exampleModal.querySelector(".modal-info-email");
+    const modalCompName = exampleModal.querySelector(".modal-info-company-name");
+    const modalCompanyTitle = exampleModal.querySelector(".modal-info-company-title");
+    const modalCompanyDepartment = exampleModal.querySelector(".modal-info-company-department");
+    const modalAddress = exampleModal.querySelector(".modal-info-address");
+
+    modalTitle.textContent = `${userFullName}`;
+    modalPhone.textContent = `${userPhone}`;
+    modalEmail.textContent = `${userEmail}`;
+    modalCompName.textContent = `${userCompName}`;
+    modalCompanyTitle.textContent = `${userCompanyTitle}`;
+    modalCompanyDepartment.textContent = `${userCompanyDepartment}`;
+    modalAddress.textContent = `Разработчики используют текст Lorem ipsum в качестве заполнителя макета страницы. Так как дополнительная информации в JSON нет, а адрес нигде не используется - закинул его сюда. ${userAddress}`;
   });
 });
 
@@ -22,7 +41,7 @@ async function getUsersList() {
 
   const result = await getUsers(apiUrl);
   const usersObj = Object.values(result)[0];
-  console.log(usersObj)
+  console.log(usersObj);
   if (usersObj === undefined || usersObj.length === 0) {
     alert("no users");
     return;
@@ -108,23 +127,32 @@ async function onSearchClick() {
   }
 }
 
-function usersTemplate(user) {
+function usersTemplate({ firstName, maidenName, lastName, phone, email, user, company, address }) {
   return `
       <div class="col-6 col-md-4 mb-3">
-        <div class="card p-3 mb-5 bg-white rounded border-0" data-bs-toggle="modal" data-bs-target="#exampleModal"  data-bs-whatever="${user.firstName} ${user.maidenName} ${user.lastName}">
+        <div class="card p-3 mb-5 bg-white rounded border-0" data-bs-toggle="modal" data-bs-target="#exampleModal" 
+        data-bs-fullname="${firstName} ${maidenName} ${lastName}" 
+        data-bs-phone="${phone}" 
+        data-bs-email="${email}" 
+        data-bs-user="${user}" 
+        data-bs-company-name="${company.name}"
+        data-bs-company-title="${company.title}"
+        data-bs-company-department="${company.department}"
+        data-bs-address="${address.address}"
+        >
             <div class="card-body">
-            <h5 class="card-title">${user.firstName} ${user.lastName}</h5>
+            <h5 class="card-title">${firstName} ${lastName}</h5>
             <div class="card-line">
                 <card-icon>
                   <div class="icon phone"></div>
                 </card-icon>
-                <p class="card-text">${user.phone}</p>
+                <p class="card-text">${phone}</p>
             </div>
             <div class="card-line" lang="en">
                 <div class="card-icon">
                   <div class="icon email"></div>
                 </div>
-                <a href="#" class="card-text" lang="en">${user.email}</a>
+                <a href="#" class="card-text" lang="en">${email}</a>
             </div>
             </div>
         </div>
@@ -132,8 +160,7 @@ function usersTemplate(user) {
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
-                <h4 class="modal-title" id="exampleModalLabel">
-                ${user.firstName} ${user.middleName} ${user.lastName} </h4>
+                <h4 class="modal-title" id="exampleModalLabel"></h4>
                 <div class="card-icon" data-bs-dismiss="modal" aria-label="Close">
                   <img src="./assets/img/x-circle-fill 1.png" alt="" class="close">
                 </div>
@@ -141,29 +168,29 @@ function usersTemplate(user) {
               <div class="modal-body">
                 <div class="row">
                   <div class="col-5"><p>Телефон:</p></div>
-                  <div class="col modal-info-phone"><p>${user.phone}</p></div>
+                  <div class="col modal-info-phone"><p></p></div>
                 </div>
                 <div class="row">
                   <div class="col-5"><p>Почта:</p></div>
-                  <div class="col modal-info-email"><p>${user.email}</p></div>
+                  <div class="col modal-info-email"><p></p></div>
                 </div>
                 <div class="row">
                   <div class="col-5"><p>Дата приема:</p></div>
-                  <div class="col modal-info-name"><p>${user.company.name}</p></div>
+                  <div class="col modal-info-company-name"><p></p></div>
                 </div>
                 <div class="row">
                   <div class="col-5"><p>Должность:</p></div>
-                  <div class="col modal-info-title"><p>${user.company.title}</p></div>
+                  <div class="col modal-info-company-title"><p></p></div>
                 </div>
                 <div class="row">
                   <div class="col-5"><p>Подразделение:</p></div>
-                  <div class="col modal-info-department"><p>${user.company.department}</p></div>
+                  <div class="col modal-info-company-department"><p></p></div>
                 </div>
               </div>
               <div class="modal-footer justify-content-start">
                 <p>Дополнительная информация:</p>
                 <div class="col modal-info-bottom">
-                  <p>Разработчики используют текст Lorem ipsum в качестве заполнителя макета страницы. Так как дополнительная информации в JSON нет, а адрес нигде не используется - закинул его сюда. ${user.address.address}</p>
+                  <p class="col modal-info-address"></p>
                 </div>
               </div>
             </div>
